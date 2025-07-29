305a3742b7ce365783d339de989ba15f75db6fe2
Uğur Tansal <ugur.tansal@gmail.com>
2025-07-29 20:19:10 +0300

    Added variable.js

---
+++ JS_Beginner/variables.js
@@ -0,0 +1,29 @@
+var name = "Ahmet";   
+console.log(name)
+
+let age = 25;   
+age=32; //It can be
+
+const country = "Türkiye"; // can not change
+country="Almanya" ; // 
+console.log(country)
+
+let city;
+console.log(city); // undefined
+
+let city2 = null;
+console.log(city2); // null
+
+
+let cities = ["Adana", "Ankara", "İstanbul"];
+let student = {
+  name: "Ahmet",
+  age: 33,
+  intern: true
+};  //object
+
+console.log(cities[0]);     // "Adana"
+console.log(student.name);      // "Ahmet"
+
+
+alert("deneme")
